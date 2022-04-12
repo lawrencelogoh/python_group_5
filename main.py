7d56bad99155657ee41d44523fe8514639ebfee8
Lawrence Logoh <lawrence.logoh@gmail.com>
2022-04-12 21:11:49 +0000

    Hello world

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+def main():
+    root = Tk()
+    root.title("Login")
+    ttk.Button(root, text="Hello World").grid()
+    root.mainloop()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
