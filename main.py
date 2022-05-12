132a242b1f5fe74d8fda49d00975fb844ebc28e8
Lawrence Logoh <lawrence.logoh@gmail.com>
2022-05-12 22:34:46 +0000

    made basic "book_type_details" and some formatting from black

--- main.py
+++ main.py
@@ -3,70 +3,94 @@ from tkinter import ttk
 
 root = Tk()
 
+
 def change_details():
+
     dash_win.withdraw()
     cd_win = Toplevel()
     cd_win.title("Add New Charges")
     cd_win.columnconfigure(0, weight=1)
     cd_win.rowconfigure(0, weight=1)
-    
-    cd_frame = ttk.LabelFrame(cd_win, text="Changes Details", padding=(3,3,12,12))
-    cd_btn_frame = ttk.Frame(cd_win, padding=(3,3,12,12))
+
+    cd_frame = ttk.LabelFrame(cd_win, text="Changes Details", padding=(3, 3, 12, 12))
+    cd_btn_frame = ttk.Frame(cd_win, padding=(3, 3, 12, 12))
     cd_frame.columnconfigure(0, weight=1)
     cd_frame.rowconfigure(0, weight=1)
-    cd_frame.grid(column=0, row=0, sticky=(N,W,E,S))
-    cd_btn_frame.grid(column=0,row=1)
+    cd_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+    cd_btn_frame.grid(column=0, row=1)
 
     change_no, change_desc, change = StringVar(), StringVar(), StringVar()
-    
+
     change_no_label = ttk.Label(cd_frame, text="Change No.")
     change_desc_label = ttk.Label(cd_frame, text="Change Desc.")
     change_label = ttk.Label(cd_frame, text="Change.")
-    
+
     change_no_field = ttk.Entry(cd_frame, width=10, textvariable=change_no)
     change_desc_field = ttk.Entry(cd_frame, width=10, textvariable=change_desc)
     change_field = ttk.Entry(cd_frame, width=10, textvariable=change)
 
-    change_no_label.grid(column=1, row=1, sticky=(W,E))
-    change_desc_label.grid(column=1, row=2, sticky=(W,E))
-    change_label.grid(column=1, row=3, sticky=(W,E))
+    change_no_label.grid(column=1, row=1, sticky=(W, E))
+    change_desc_label.grid(column=1, row=2, sticky=(W, E))
+    change_label.grid(column=1, row=3, sticky=(W, E))
 
     change_no_field.grid(column=2, row=1)
     change_desc_field.grid(column=2, row=2)
     change_field.grid(column=2, row=3)
 
-    
-    add_button= ttk.Button(cd_btn_frame, text="Add")
-    ignore_button= ttk.Button(cd_btn_frame, text="Ignore")
-    exit_button= ttk.Button(cd_btn_frame, text="Exit")
+    add_button = ttk.Button(cd_btn_frame, text="Add")
+    ignore_button = ttk.Button(cd_btn_frame, text="Ignore")
+    exit_button = ttk.Button(cd_btn_frame, text="Exit")
 
     add_button.grid(column=1, row=1)
     ignore_button.grid(column=2, row=1)
     exit_button.grid(column=3, row=1)
 
-    
-
     for child in cd_frame.winfo_children():
         child.grid_configure(padx=5, pady=5)
 
-    
 
 def book_type_details():
     dash_win.withdraw()
     bt_win = Toplevel()
     bt_win.title("Add New Book Type")
     bt_frame = ttk.LabelFrame(bt_win, text="Book Type Details", padding="3 3 12 12")
+    bt_btn_frame = ttk.Frame(bt_win, padding="3 3 12 12")
 
-    for child in cd_frame.winfo_children():
+    type_no_label = ttk.Label(bt_frame, text="Type No.")
+    desc_label = ttk.Label(bt_frame, text="Description")
+
+    type_no, desc = StringVar(), StringVar()
+    type_no_field = ttk.Entry(bt_frame, textvariable=type_no)
+    desc_field = ttk.Entry(bt_frame, textvariable=desc)
+
+    add_button = ttk.Button(bt_btn_frame, text="Add")
+    ignore_button = ttk.Button(bt_btn_frame, text="Ignore")
+    exit_button = ttk.Button(bt_btn_frame, text="Exit")
+
+    bt_frame.grid(column=0, row=0, sticky=(N, W, E, S), padx=10, pady=10)
+    bt_btn_frame.grid(column=0, row=1)
+
+    type_no_label.grid(column=0, row=0)
+    desc_label.grid(column=0, row=1)
+
+    type_no_field.grid(column=1, row=0, columnspan=2)
+    desc_field.grid(column=1, row=1, columnspan=2)
+
+    add_button.grid(column=0, row=0)
+    ignore_button.grid(column=1, row=0)
+    exit_button.grid(column=2, row=0)
+
+    for child in bt_frame.winfo_children():
         child.grid_configure(padx=5, pady=5)
-        
+
+
 def book_code_details():
     dash_win.withdraw()
     bc_win = Toplevel()
     bc_win.title("Add New Book Code")
     bc_frame = ttk.LabelFrame(bc_win, text="Book Code Details", padding="3 3 12 12")
 
-    
+
 def dashboard():
     root.withdraw()
     global dash_win
@@ -74,23 +98,26 @@ def dashboard():
     dash_win.title("Admin Dashboard")
     dash_frame = ttk.LabelFrame(dash_win, text="Admin Dashboard", padding="3 3 12 12")
     dash_frame.grid(column=0, row=0, sticky=(N, W, E, S))
-    
-    new_charges= ttk.Button(dash_frame, text="Add New Charges", command=change_details)
-    new_book_type= ttk.Button(dash_frame, text="Add New Book Type", command=book_type_details)
-    new_book_code= ttk.Button(dash_frame, text="Add New Book Code", command=book_code_details)
+
+    new_charges = ttk.Button(dash_frame, text="Add New Charges", command=change_details)
+    new_book_type = ttk.Button(
+        dash_frame, text="Add New Book Type", command=book_type_details
+    )
+    new_book_code = ttk.Button(
+        dash_frame, text="Add New Book Code", command=book_code_details
+    )
 
     for i, child in enumerate(dash_frame.winfo_children()):
-        child.grid(column=1, row=i, sticky=(N,W,E,S))
+        child.grid(column=1, row=i, sticky=(N, W, E, S))
         child.grid_configure(padx=5, pady=5)
 
-    
-    
-    dash_win.bind("<Destroy>", lambda e: root.state('normal'))
+    dash_win.bind("<Destroy>", lambda e: root.state("normal"))
+
 
 def login():
 
     root.title("Login")
-    
+
     mainframe = ttk.Frame(root, padding="3 3 12 12")
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
@@ -103,11 +130,11 @@ def login():
 
     username_label = ttk.Label(mainframe, text="Username")
     password_label = ttk.Label(mainframe, text="Password")
-    login_btn = ttk.Button(mainframe, text="Login",command=dashboard)
-    
+    login_btn = ttk.Button(mainframe, text="Login", command=dashboard)
+
     username_label.grid(column=1, row=1, sticky=W)
     password_label.grid(column=1, row=2, sticky=W)
-    login_btn.grid(column=1, row=3, columnspan=2,sticky=S)
+    login_btn.grid(column=1, row=3, columnspan=2, sticky=S)
 
     for i, child in enumerate(mainframe.winfo_children()):
         mainframe.columnconfigure(i, weight=1)
@@ -117,12 +144,10 @@ def login():
         child.grid_configure(padx=5, pady=5)
     root.mainloop()
 
+
 def main():
-      login()
-    
+    login()
 
 
 if __name__ == "__main__":
     main()
-
-    
